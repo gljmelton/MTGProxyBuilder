0364d7a501b67439914c0fab36a56ad63ca3b82c
gljmelton <gljmelton@gmail.com>
2026-09-24 20:16:34 -0700

    Long lines and faces pt. 1
    - Added some preliminary support for generating pages for cards with multiple faces
    - Improved PDF export to proper 8.5x11 size
    - Added layout improvements for cards with long names, type lines, and oracle text.

--- layout_generator.py
+++ layout_generator.py
@@ -1,9 +1,17 @@
 import os
 import re
 import textwrap
+import img2pdf
 from enum import Enum
+from pathlib import Path
+
+from PIL.Image import Image, Resampling
+
+from scryfall import scryfall
 from scryfall.card import Card
 from pictex import Canvas, Row, Column, Text
+
+from scryfall.scryfall import Scryfall, Faces
 
 WIDTH = 8.5
 HEIGHT = 11.0
@@ -13,10 +21,11 @@ POINT_SIZE = (DPI / 72.0)
 PIXEL_WIDTH = int(WIDTH * DPI)
 PIXEL_HEIGHT = int(HEIGHT * DPI)
 
-BIG_SIZES = [12, 9, 7]
-REGULAR_SIZES = [9, 7]
-SMALL_SIZES = [7, 5]
-ONLY_SMALL = [7]
+BIG_SIZES = [18, 14, 12]
+REGULAR_SIZES = [12, 9]
+SMALL_SIZES = [9, 7]
+ONLY_SMALL = [9]
+ONLY_TINY = [7]
 
 class Casing(Enum):
     DEFAULT = "Default"
@@ -50,8 +59,8 @@ class LayoutGenerator:
         self.fonts = [
             FontSet("SansSerif", "BeVietnamPro-Regular.ttf"),
             FontSet("Serif", "Alegreya-Regular.ttf"),
-            FontSet("Blackletter", "PirataOne-Regular.ttf"),
             FontSet("Overprint", "Overprint TM.ttf"),
+            FontSet("Blackletter", "PirataOne-Regular.ttf"),
             FontSet("Block", "Staatliches-Regular.ttf"),
             FontSet("Monospace", "FiraCode-Regular.ttf"),
             FontSet("Typewriter", "Mom_typewrite.ttf"),
@@ -101,36 +110,39 @@ class LayoutGenerator:
         col = []
         #Name
         if self.custom_data["nickname"] == "":
-            col.append(self.generate_data_row(self.format_name(self.card.name), fonts, BIG_SIZES, FontWeight.Bold))
+            col.append(self.generate_data_row(self.format_name(Scryfall.get_card_name(self.card)), fonts, BIG_SIZES, FontWeight.Bold))
         else:
             col.append(self.generate_data_row(self.format_name(self.custom_data["nickname"]), fonts, BIG_SIZES, FontWeight.Bold))
-            col.append(self.generate_data_row(self.format_name(self.card.name), fonts, SMALL_SIZES, FontWeight.Italic))
+            col.append(self.generate_data_row(self.format_name(Scryfall.get_card_name(self.card)), fonts, SMALL_SIZES, FontWeight.Italic))
 
         #Type
-        col.append(self.generate_data_row(self.format_type_line(self.card.type_line), fonts, REGULAR_SIZES, FontWeight.Regular))
+        col.append(self.generate_data_row(self.format_type_line(Scryfall.get_type_line(self.card)), fonts, REGULAR_SIZES, FontWeight.Regular))
 
         #Oracle
+        oracle_sizes = REGULAR_SIZES
+        if len(Scryfall.get_card_oracle(self.card)) > 250:
+            oracle_sizes = ONLY_TINY
         if self.card.type_line == "Dungeon":
             col.append(self.generate_data_row(
-                self.format_oracle(self.card.oracle_text),  # Wrap text and remove any reminder text
+                self.format_oracle(Scryfall.get_card_oracle(self.card, Faces.Front)),  # Wrap text and remove any reminder text
                 fonts, ONLY_SMALL, FontWeight.Regular))
         else:
             col.append(self.generate_data_row(
-                self.format_oracle(self.card.oracle_text), #Wrap text and remove any reminder text
-                fonts, REGULAR_SIZES, FontWeight.Regular))
+                self.format_oracle(Scryfall.get_card_oracle(self.card, Faces.Front)), #Wrap text and remove any reminder text
+                fonts, oracle_sizes, FontWeight.Regular))
 
         row = []
         #Mana Value
-        if self.card.mana_cost:
-            row.append(self.generate_data_row(self.format_mana(self.card.mana_cost), fonts, REGULAR_SIZES,
+        if Scryfall.get_mana_value(self.card):
+            row.append(self.generate_data_row(self.format_mana(Scryfall.get_mana_value(self.card)), fonts, REGULAR_SIZES,
                                        FontWeight.Regular))
         #Power/Toughness
-        if self.card.power and self.card.toughness:
-            power = self.card.power
+        if Scryfall.get_power(self.card, Faces.Front) and Scryfall.get_toughness(self.card, Faces.Front):
+            power = Scryfall.get_power(self.card, Faces.Front)
             if not self.custom_data["power"] == "":
                 power = self.custom_data["power"]
 
-            toughness = self.card.toughness
+            toughness = Scryfall.get_toughness(self.card, Faces.Front)
             if not self.custom_data["toughness"] == "":
                 toughness = self.custom_data["toughness"]
 
@@ -141,17 +153,21 @@ class LayoutGenerator:
             *col,
             Row(*row)
         )
-        col.padding(0.25 * DPI)
+        col.padding(0.15 * DPI)
 
         canvas = Canvas()
         canvas.size(PIXEL_WIDTH, PIXEL_HEIGHT)
         canvas.background_color("white")
-        img = canvas.render(col).to_pillow()
+        img = canvas.render(col).to_pillow().convert("RGB")
 
-        pdf_path = rf"output\{self.card.name}.pdf"
+        #img = img.resize((int(img.size[0] / 3), int(img.size[1] / 3)), Resampling.LANCZOS)
+        pdf_path = rf"output\{Scryfall.get_card_name(self.card)}.pdf"
+        img.save(rf"output\{Scryfall.get_card_name(self.card)}.png", "PNG")
 
         try:
-            img.save(pdf_path, "PDF",resolution=100.0, save_all=True)
+            layout = img2pdf.get_fixed_dpi_layout_fun((DPI, DPI))
+            Path(pdf_path).write_bytes(img2pdf.convert(rf"output\{Scryfall.get_card_name(self.card)}.png", layout_fun=layout))
+            #img.save(pdf_path, "PDF",resolution=100.0, save_all=True)
             os.startfile(rf"{pdf_path}")
             return True
         except PermissionError:
@@ -178,6 +194,8 @@ class LayoutGenerator:
 
     def generate_data_set(self, text, font : FontSet, size, weight : FontWeight):
         print(f"Generating data for font {font.name}")
+        if size >= 14:
+            text = self.wrap_text(18, text)
         font = self.get_weight_for_font(font, weight)
         regular = Text(text)
         self.style_text(regular, font, size)
@@ -234,6 +252,8 @@ class LayoutGenerator:
         return text
 
     def format_type_line(self, text: str):
+        if len(text) > 20:
+            text = text.replace(" \u2014 ", "\n")
         return text
 
     def format_mana(self, text: str):
--- scryfall/scryfall.py
+++ scryfall/scryfall.py
@@ -1,11 +1,16 @@
 import msgspec
 import requests
-from .card import Card
+from .card import Card, Layout
 from rapidfuzz import fuzz
 from PIL import Image
 from io import BytesIO
+from enum import Enum
 
 SEARCH_RATIO = 60
+
+class Faces(Enum):
+    Front = 0
+    Back = 1
 
 class Scryfall:
     def __init__(self, bulk_data_path):
@@ -22,12 +27,12 @@ class Scryfall:
 
     #HTTP
     def get_card_image(self, card : Card):
-        print(f"[Scryfall][get_card_image] requesting card image {card.image_uris.normal}")
+        print(f"[Scryfall][get_card_image] requesting card image {self.get_card_image_uri(card)}")
         headers = {
             "User-Agent": "ProxyBuilder",
             "Accept": "*/*"
         }
-        response = requests.get(card.image_uris.normal, headers=headers)
+        response = requests.get(self.get_card_image_uri(card), headers=headers)
 
         if response.status_code != 200:
             print(f"[Scryfall][get_card_image] Failed to get card image | Response: {response.status_code}")
@@ -44,10 +49,62 @@ class Scryfall:
         search_result = None
         best_score = 0
         for card in self.bulk_data:
-            ratio = fuzz.ratio(name.lower(), card.name.lower())
+            if card.layout == Layout.ART_SERIES:
+                continue
+
+            ratio = fuzz.ratio(name.lower(), self.get_card_name(card).lower())
             if ratio >= SEARCH_RATIO and ratio > best_score:
-                print(f"[Scryfall][search_by_name] Found better card: {card.name}")
+                print(f"[Scryfall][search_by_name] Found better card: {self.get_card_name(card)}")
                 best_score = ratio
                 search_result = card
 
         return search_result
+
+    @staticmethod
+    def get_card_name(card, face: Faces= Faces.Front):
+        if card.card_faces:
+            return card.card_faces[face.value].name
+        else :
+            return card.name
+
+    @staticmethod
+    def get_card_image_uri(card: Card, face:Faces = Faces.Front):
+        if card.card_faces:
+            return card.card_faces[face.value].image_uris.normal
+        else :
+            return card.image_uris.normal
+
+    @staticmethod
+    def get_card_oracle(card: Card, face: Faces = Faces.Front):
+        if card.card_faces:
+            return card.card_faces[face.value].oracle_text
+        else :
+            return card.oracle_text
+
+    @staticmethod
+    def get_type_line(card: Card, face: Faces = Faces.Front):
+        if card.card_faces:
+            return card.card_faces[face.value].type_line
+        else :
+            return card.type_line
+
+    @staticmethod
+    def get_mana_value(card: Card, face: Faces = Faces.Front):
+        if card.card_faces:
+            return card.card_faces[face.value].mana_cost
+        else :
+            return card.mana_cost
+
+    @staticmethod
+    def get_power(card: Card, face: Faces = Faces.Front):
+        if card.card_faces:
+            return card.card_faces[face.value].power
+        else :
+            return card.power
+
+    @staticmethod
+    def get_toughness(card: Card, face: Faces = Faces.Front):
+        if card.card_faces:
+            return card.card_faces[face.value].toughness
+        else :
+            return card.toughness
